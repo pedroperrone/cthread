6038f23fd94c9a6b74ffd734dc94ad5f6f9f75a3
Pedro Perrone <psperrone@hotmail.com>
2018-09-16 22:16:33 -0300

    Create tests to priority

--- teste/cthread_test.c
+++ teste/cthread_test.c
@@ -1,12 +1,24 @@
+// Expected output:
+// 	I am the main thread.
+// 	Thread 1, with priority 0, starts.
+// 	Thread 1 ends.
+// 	Thread 2, with priority 2, starts.
+// 	Thread 3, with priority 1, starts.
+// 	Thread 3 ends.
+// 	Thread 2 ends.
+// 	I am the main thread. Now that all the created threads are finished, I execute again.
+
 #include <stdio.h>
 #include "../include/cthread.h"
 
-void thread();
+void thread_one();
+void thread_two();
+void thread_three();
 
 int main() {
 	int thread_tcb;
 	printf("I am the main thread.\n");
-	thread_tcb = ccreate((void*) thread, NULL, 1);
+	thread_tcb = ccreate((void*) thread_one, NULL, 0);
 	if(thread_tcb == -1) {
 		printf("Error creating the new thread.\n");
 	}
@@ -14,6 +26,27 @@ int main() {
 	return 0;
 }
 
-void thread() {
-	printf("This is the new thread.\n");
+void thread_one() {
+	int tcb;
+	printf("Thread 1, with priority 0, starts.\n");
+	tcb = ccreate((void*) thread_two, NULL, 2);
+	if(tcb == -1) {
+		printf("Error creating thread 2.\n");
+	}
+	printf("Thread 1 ends.\n");
+}
+
+void thread_two() {
+	int tcb;
+	printf("Thread 2, with priority 2, starts\n");
+	tcb = ccreate((void*) thread_three, NULL, 1);
+	if(tcb == -1) {
+		printf("Error creating thread 3\n");
+	}
+	printf("Thread 2 ends.\n");
+}
+
+void thread_three() {
+	printf("Thread 3, with priority 1, starts\n");
+	printf("Thread 3 ends.\n");
 }

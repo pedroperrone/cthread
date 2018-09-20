be3cd2105580849fb7bbf6b0fba05c4eb7870016
Pedro Perrone <psperrone@hotmail.com>
2018-09-20 13:11:50 -0300

    Add a test case for cjoin function (#11)

--- teste/cjoin_test.c
+++ teste/cjoin_test.c
@@ -8,6 +8,12 @@
 	// Thread 2 joined thread with same priority.
 	// Thread 2, with priority 1, ends.
 	// Thread 1 joined thread with lower priority.
+	// Thread 1 create threads 4 and 5 and yields.
+	// Thread 4 yields so 5 can join.
+	// Thread 5 joins thread 4.
+	// Thread 1 joins 5
+	// Thread 4 ends and frees thread 5
+	// Thread 5 ends ends and frees thread 1.
 	// Thread 1 ends.
 	// Mains join should return error because new thread is ended.
 
@@ -17,6 +23,8 @@
 void thread_one();
 void thread_two();
 void thread_three();
+void thread_four();
+void thread_five(int *tid);
 
 int main() {
 	int thread_tcb = ccreate((void*) thread_one, NULL, 0);
@@ -32,7 +40,7 @@ int main() {
 }
 
 void thread_one() {
-	int tid;
+	int tid, tid_two;
 	printf("Thread 1, with priority 0, starts.\n");
 	tid = ccreate((void*) thread_two, NULL, 1);
 	if(tid == -1) {
@@ -43,6 +51,18 @@ void thread_one() {
 		printf("ERROR on join.\n");
 	}
 	printf("Thread 1 joined thread with lower priority.\n");
+	tid = ccreate((void*) thread_four, NULL, 0);
+	if(tid == -1) {
+		printf("UNEXPECTED error creating thread.\n");
+	}
+	tid_two = ccreate((void*) thread_five, &tid, 0);
+	if(tid_two == -1) {
+		printf("UNEXPECTED error creating thread.\n");
+	}
+	printf("Thread 1 create threads 4 and 5 and yields.\n");
+	cyield();
+	printf("Thread 1 joins 5\n");
+	cjoin(tid_two);
 	printf("Thread 1 ends.\n");
 }
 
@@ -64,4 +84,17 @@ void thread_two() {
 void  thread_three() {
 	printf("Thread 3, with priority 1, starts.\n");
 	printf("Thread 3 ends.\n");
+}
+
+void thread_four() {
+	printf("Thread 4 yields so 5 can join.\n");
+	cyield();
+	printf("Thread 4 ends and frees thread 5\n");
+
+}
+
+void thread_five(int *tid) {
+	printf("Thread 5 joins thread 4.\n");
+	cjoin(*tid);
+	printf("Thread 5 ends ends and frees thread 1.\n");
 }
